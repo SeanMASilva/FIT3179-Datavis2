26c7ff9d15b2bf342b6c84fa9db930c4c2f6b70d
Sean Silva <seansilva003@gmail.com>
2025-10-19 22:34:49 +1100

    Ship it

--- src/Chart.jsx
+++ src/Chart.jsx
@@ -1,13 +1,10 @@
-import { useEffect } from "react";
 import embed from "vega-embed";
 import { hash } from "vega-lite";
 
 function Chart({spec, style}) {
   const id = "vega-" +  hash(spec)
-  useEffect(() => {
-    embed("#" + id, spec)
-  })
-  return <div id={id} style={style}></div>
+  embed("#" + id, spec)
+  return <div id={id} style={style} className="chart"></div>
 }
 
 export default Chart
