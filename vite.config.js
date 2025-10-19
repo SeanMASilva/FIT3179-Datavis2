590700a9646aec2f69785c32c4afdc54559a1e58
Sean Silva <seansilva003@gmail.com>
2025-10-19 22:54:07 +1100

    Fix root?

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "https://SeanMASilva.github.io/FIT3179-Datavis-2"
+  base: "https://SeanMASilva.github.io/FIT3179-Datavis2"
 })
